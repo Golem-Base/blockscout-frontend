b0e3946e06222bb05e37331639e0ea46fceca116
Bartosz Buczkowski <bartosz.r.buczkowski@gmail.com>
2025-09-03 15:26:46 +0200

    feature/GBBE-196 Add mobile version for related entity results table

--- ui/entity/EntityResultsTable.tsx
+++ ui/entity/EntityResultsTable.tsx
@@ -39,71 +39,71 @@ const EntityResultsTable = ({
   }
 
   return (
-    <>
-      <Box hideFrom="lg">
-        { displayedItems.map((item, index) => (
-          <Skeleton
-            key={ (isLoading ? 'placeholder_' : 'actual_') + index }
-            loading={ isLoading }
-            display="block"
-            whiteSpace="nowrap"
-            overflow="hidden"
-            mb={ 1 }
-          >
-            { item.key }
-          </Skeleton>
-        )) }
-      </Box>
-      <Box hideBelow="lg">
-        <TableRoot fontWeight={ 500 }>
-          <TableHeaderSticky
-            top={ pagination.isVisible ? ACTION_BAR_HEIGHT_DESKTOP : 0 }
-          >
-            <TableRow>
-              <TableColumnHeader>Search result</TableColumnHeader>
-              <TableColumnHeader width="150px">Status</TableColumnHeader>
-            </TableRow>
-          </TableHeaderSticky>
-          <TableBody>
-            { displayedItems.map((item, index) => (
-              <TableRow key={ (isLoading ? 'placeholder_' : 'actual_') + index }>
-                <TableCell
-                  fontSize="sm"
-                  textTransform="capitalize"
-                  verticalAlign="middle"
-                >
-                  <Flex>
-                    <Skeleton
-                      loading={ isLoading }
-                      borderRadius="full"
-                      boxSize={ 5 }
-                      mr={ 2 }
-                      display="inline-flex"
-                      alignItems="center"
-                      justifyContent="center"
-                      minW={ 5 }
-                      minH={ 5 }
+    <Box overflowX="auto" width="100%">
+      <TableRoot fontWeight={ 500 } minWidth="700px">
+        <TableHeaderSticky
+          top={ pagination.isVisible ? ACTION_BAR_HEIGHT_DESKTOP : 0 }
+        >
+          <TableRow>
+            <TableColumnHeader width="80%">Search result</TableColumnHeader>
+            <TableColumnHeader width="20%" minWidth="100px" maxWidth="120px" textAlign="center">Status</TableColumnHeader>
+          </TableRow>
+        </TableHeaderSticky>
+        <TableBody>
+          { displayedItems.map((item, index) => (
+            <TableRow key={ (isLoading ? 'placeholder_' : 'actual_') + index }>
+              <TableCell
+                fontSize="sm"
+                textTransform="capitalize"
+                verticalAlign="middle"
+                width="70%"
+                maxWidth="0"
+                overflow="hidden"
+                textOverflow="ellipsis"
+                whiteSpace="nowrap"
+              >
+                <Flex>
+                  <Skeleton
+                    loading={ isLoading }
+                    borderRadius="full"
+                    boxSize={ 5 }
+                    mr={ 2 }
+                    display="inline-flex"
+                    alignItems="center"
+                    justifyContent="center"
+                    minW={ 5 }
+                    minH={ 5 }
+                  >
+                    <IconSvg name="docs" boxSize={ 5 } color="text.secondary"/>
+                  </Skeleton>
+                  <Skeleton loading={ isLoading } minW="120px">
+                    <Link
+                      href={ `/entity/${ item.key }` }
+                      overflow="hidden"
+                      whiteSpace="nowrap"
+                      display="block"
                     >
-                      <IconSvg name="docs" boxSize={ 5 } color="text.secondary"/>
-                    </Skeleton>
-                    <Skeleton loading={ isLoading } minW="120px">
-                      <Link href={ `/entity/${ item.key }` }>{ item.key }</Link>
-                    </Skeleton>
-                  </Flex>
-                </TableCell>
-                <TableCell
-                  fontSize="sm"
-                  textTransform="capitalize"
-                  verticalAlign="middle"
-                >
-                  <EntityStatus isLoading={ isLoading } status={ item.status }/>
-                </TableCell>
-              </TableRow>
-            )) }
-          </TableBody>
-        </TableRoot>
-      </Box>
-    </>
+                      { item.key }
+                    </Link>
+                  </Skeleton>
+                </Flex>
+              </TableCell>
+              <TableCell
+                fontSize="sm"
+                textTransform="capitalize"
+                verticalAlign="middle"
+                width="30%"
+                minWidth="140px"
+                maxWidth="180px"
+                textAlign="center"
+              >
+                <EntityStatus isLoading={ isLoading } status={ item.status }/>
+              </TableCell>
+            </TableRow>
+          )) }
+        </TableBody>
+      </TableRoot>
+    </Box>
   );
 };
 
