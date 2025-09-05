7e6f0f0d69697cbb0f5d0e5c9562cc70f5347585
Bartosz Buczkowski <bartosz.r.buczkowski@gmail.com>
2025-09-05 11:25:41 +0200

    feature/GBBE-155 Add switching between chains with no-dynamic page context

---
+++ ui/snippets/networkMenu/utils.ts
@@ -0,0 +1,19 @@
+import type { useRouter } from 'next/router';
+
+const dynamicRouteSlugRegex = /\[.+?\]/;
+const slashEndOfStringRegex = /\/$/;
+
+interface GetStaticNetworkPathAttributes {
+  url: string;
+  router: ReturnType<typeof useRouter>;
+}
+
+export const getStaticNetworkPath = ({ url, router }: GetStaticNetworkPathAttributes) => {
+  const isDynamicRoute = dynamicRouteSlugRegex.test(router.pathname);
+  const urlWithoutSlash = url.replace(slashEndOfStringRegex, '');
+  const href = isDynamicRoute ? url : urlWithoutSlash + router.pathname;
+  const searchParams = router.asPath.split('?')[1];
+  const hrefWithSearchParams = href + `?${ searchParams }`;
+
+  return hrefWithSearchParams;
+};
