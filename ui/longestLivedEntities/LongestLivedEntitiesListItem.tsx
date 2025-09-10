f20c785d727e9f58f72b488ff35aae363a9de706
Bartosz Buczkowski <bartosz.r.buczkowski@gmail.com>
2025-09-10 08:23:07 +0200

    feature/GBBE-147

--- ui/longestLivedEntities/LongestLivedEntitiesListItem.tsx
+++ ui/longestLivedEntities/LongestLivedEntitiesListItem.tsx
@@ -1,14 +1,10 @@
-import { chakra, Flex } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
 import React from 'react';
 
 import type { EntityWithExpTimestamp } from '@golembase/l3-indexer-types';
 
-import { route } from 'nextjs-routes';
-
-import { Link } from 'toolkit/chakra/link';
 import { Skeleton } from 'toolkit/chakra/skeleton';
-import { TruncatedTextTooltip } from 'toolkit/components/truncation/TruncatedTextTooltip';
-import IconSvg from 'ui/shared/IconSvg';
+import StorageEntity from 'ui/shared/entities/entity/StorageEntity';
 import ListItemMobile from 'ui/shared/ListItemMobile/ListItemMobile';
 
 import DetailedInfoTimestamp from './LongestLivedEntitiesExpirationTime';
@@ -28,45 +24,7 @@ const LongestLivedEntitiesListItem = ({
   return (
     <ListItemMobile>
       <Flex justifyContent="space-between" w="100%" gap={ 6 }>
-        <Flex width="100%" alignItems="center" minWidth={ 0 }>
-          <Skeleton
-            loading={ isLoading }
-            borderRadius="full"
-            boxSize={ 5 }
-            mr={ 2 }
-            display="inline-flex"
-            alignItems="center"
-            justifyContent="center"
-            flexShrink={ 0 }
-          >
-            <IconSvg name="docs" boxSize={ 5 } color="text.secondary"/>
-          </Skeleton>
-
-          <Skeleton loading={ isLoading } flex="1" minWidth={ 0 }>
-            <Link
-              href={ route({ pathname: '/entity/[key]', query: { key: item.key } }) }
-              display="block"
-              minWidth={ 0 }
-              maxWidth="100%"
-              overflow="hidden"
-              whiteSpace="nowrap"
-            >
-              <TruncatedTextTooltip
-                label={ item.key }
-                interactive={ true }
-              >
-                <chakra.span
-                  display="block"
-                  overflow="hidden"
-                  textOverflow="ellipsis"
-                  whiteSpace="nowrap"
-                >
-                  { item.key }
-                </chakra.span>
-              </TruncatedTextTooltip>
-            </Link>
-          </Skeleton>
-        </Flex>
+        <StorageEntity entityKey={ item.key } isLoading={ isLoading } truncate="dynamic"/>
 
         <Skeleton loading={ isLoading } fontSize="sm" ml="auto" minW={ 6 } color="text.secondary">{ rank }</Skeleton>
       </Flex>
