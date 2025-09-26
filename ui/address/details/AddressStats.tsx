868194f2616046c94a8b4e358342e1ca0f8f9a88
Mateusz Juszczyk <matixovi@gmail.com>
2025-09-26 10:01:56 +0200

    Add ranks stub

--- ui/address/details/AddressStats.tsx
+++ ui/address/details/AddressStats.tsx
@@ -1,15 +1,22 @@
 import { Flex } from '@chakra-ui/react';
 import React from 'react';
 
+import type { Route } from 'nextjs-routes';
+import { route } from 'nextjs-routes';
+
 import useApiQuery from 'lib/api/useApiQuery';
 import formatDataSize from 'lib/formatDataSize';
 import { formatBigNum } from 'lib/web3/formatBigNum';
-import { ADDRESS_STATS } from 'stubs/address';
+import { ADDRESS_RANKS, ADDRESS_STATS } from 'stubs/address';
+import { Link } from 'toolkit/chakra/link';
 import { Skeleton } from 'toolkit/chakra/skeleton';
 import * as DetailedInfo from 'ui/shared/DetailedInfo/DetailedInfo';
 import { ItemDivider } from 'ui/shared/DetailedInfo/DetailedInfo';
 import DetailedInfoTimestamp from 'ui/shared/DetailedInfo/DetailedInfoTimestamp';
 import BlockEntity from 'ui/shared/entities/block/BlockEntity';
+
+const PAGE_SIZE = 50;
+type LeaderboardRoute = Extract<Route, { pathname: `/leaderboards/${ string }` }>['pathname'];
 
 interface Props {
   addressHash: string;
@@ -28,12 +35,12 @@ const AddressStats = ({ addressHash, isLoading }: Props) => {
     },
   });
 
-  const ranksQuery = useApiQuery('golemBaseIndexer:addressLeaderboardRanks', {
+  const { data: ranksData, isPlaceholderData: isLoadingRanks } = useApiQuery('golemBaseIndexer:addressLeaderboardRanks', {
     pathParams: { address: addressHash },
     queryOptions: {
       enabled: Boolean(addressHash),
       refetchOnMount: false,
-      // placeholderData: ADDRESS_STATS,
+      placeholderData: ADDRESS_RANKS,
     },
   });
 
@@ -41,7 +48,7 @@ const AddressStats = ({ addressHash, isLoading }: Props) => {
     return null;
   }
 
-  const loading = isLoading || isPlaceholderData;
+  const loading = isLoading || isPlaceholderData || isLoadingRanks;
   const renderItem = (label: string, hint: string, value: React.ReactNode) => (
     <>
       <DetailedInfo.ItemLabel
@@ -57,6 +64,23 @@ const AddressStats = ({ addressHash, isLoading }: Props) => {
       </DetailedInfo.ItemValue>
     </>
   );
+
+  const renderRankItem = (label: string, hint: string, rank: string | undefined, pathname: LeaderboardRoute) => {
+    const rankInt = rank ? parseInt(rank, 10) : 0;
+    if (rankInt === 0) {
+      return null;
+    }
+
+    const pageNum = rankInt / PAGE_SIZE;
+
+    return renderItem(
+      label,
+      hint,
+      <Link href={ route({ pathname, query: (pageNum > 1 ? { page: String(pageNum) } : {}) }) }>
+        #{ formatBigNum(rank) }
+      </Link>,
+    );
+  };
 
   return (
     <>
@@ -156,6 +180,36 @@ const AddressStats = ({ addressHash, isLoading }: Props) => {
           number={ data.last_seen_block }
           isLoading={ isLoading }
         />,
+      ) }
+
+      <ItemDivider/>
+
+      { renderRankItem(
+        'Biggest Spenders Rank',
+        'Rank in the biggest spenders leaderboard based on total transaction fees paid',
+        ranksData?.biggest_spenders,
+        '/leaderboards/spenders',
+      ) }
+
+      { renderRankItem(
+        'Entities Created Rank',
+        'Rank in the top entity creators leaderboard based on number of entities created',
+        ranksData?.entities_created,
+        '/leaderboards/entity-creators',
+      ) }
+
+      { renderRankItem(
+        'Entities Owned Rank',
+        'Rank in the top entity owners leaderboard based on number of entities currently owned',
+        '200',
+        '/leaderboards/owners',
+      ) }
+
+      { renderRankItem(
+        'Data Owned Rank',
+        'Rank in the largest entities leaderboard based on total data size owned',
+        ranksData?.data_owned,
+        '/leaderboards/largest-entities',
       ) }
     </>
   );
