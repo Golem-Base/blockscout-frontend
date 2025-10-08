051efbeb7ac9ff420ca0a308c019aaa509ac12d7
Bartosz Buczkowski <bartosz.r.buczkowski@gmail.com>
2025-10-08 09:26:56 +0200

    feature/GBBE-147 Fix display timestamps greater than Number.MAX_SAFE_INTIGER

--- ui/longestLivedEntities/LongestLivedEntitiesExpirationTime.tsx
+++ ui/longestLivedEntities/LongestLivedEntitiesExpirationTime.tsx
@@ -1,5 +1,4 @@
 import { Flex } from '@chakra-ui/react';
-import { isNil } from 'es-toolkit';
 import React from 'react';
 
 import dayjs from 'lib/date/dayjs';
@@ -8,17 +7,41 @@ import { Tooltip } from 'toolkit/chakra/tooltip';
 import IconSvg from 'ui/shared/IconSvg';
 
 type Props = {
-  timestamp?: string | number | null;
+  expiresAtTimestampSec: string;
   isLoading?: boolean;
 };
 
-const LongestLivedEntitiesExpirationTime = ({ timestamp, isLoading }: Props) => {
+/**
+ * @param {string | number | bigint} baseMs - base timestamp in milliseconds
+ * @param {string | number | bigint} addSec - seconds to add
+ * @returns {dayjs.Dayjs} safely computed date
+ */
+function safeAddTimestamp(baseMs: string | number | bigint, addSec: string | number | bigint): dayjs.Dayjs {
+  const base = BigInt(baseMs);
+  const add = BigInt(addSec) * BigInt(1000);
+
+  const sum = base + add;
+
+  if (sum > BigInt(Number.MAX_SAFE_INTEGER)) {
+    const MAX_DATE_MS = BigInt(8640000000000000);
+    const safeDate = new Date(Number(sum % MAX_DATE_MS));
+
+    return dayjs(safeDate);
+  }
+
+  return dayjs(Number(sum));
+}
+
+const LongestLivedEntitiesExpirationTime = ({ expiresAtTimestampSec, isLoading }: Props) => {
+  const now = Date.now();
+  const date = safeAddTimestamp(now, expiresAtTimestampSec);
+
   return (
     <Skeleton loading={ isLoading } cursor="pointer">
-      <Tooltip content={ dayjs(timestamp).format('llll') }>
+      <Tooltip content={ date.format('llll') }>
         <Flex alignItems="center" gap={ 2 }>
           <IconSvg name="clock" boxSize={ 4 } color="gray.500" isLoading={ isLoading }/>
-          { isNil(timestamp) ? 'Far in the future' : dayjs(timestamp).fromNow() }
+          { date.fromNow() }
         </Flex>
       </Tooltip>
     </Skeleton>
