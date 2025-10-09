8f2e348f9d05d84c18fd54f1f20ba4a1a1ffe579
Bartosz Buczkowski <bartosz.r.buczkowski@gmail.com>
2025-10-09 11:07:53 +0200

    feature/GBBE-251 Del unnecessary block of code

--- ui/address/addressOwnedFilterSchema.ts
+++ ui/address/addressOwnedFilterSchema.ts
@@ -8,19 +8,19 @@ export const addressOwnedFilterSchema = v.pipe(
     string_annotation_value: v.string(),
   }),
   v.forward(
-    v.check((input) => !(Boolean(input.numeric_annotation_key.length) && !input.numeric_annotation_value.length)),
+    v.check((input) => !(input.numeric_annotation_key && !input.numeric_annotation_value)),
     [ 'numeric_annotation_value' ],
   ),
   v.forward(
-    v.check((input) => !(Boolean(input.numeric_annotation_value.length) && !input.numeric_annotation_key.length)),
+    v.check((input) => !(input.numeric_annotation_value && !input.numeric_annotation_key)),
     [ 'numeric_annotation_key' ],
   ),
   v.forward(
-    v.check((input) => !(Boolean(input.string_annotation_key.length) && !input.string_annotation_value.length)),
+    v.check((input) => !(input.string_annotation_key && !input.string_annotation_value)),
     [ 'string_annotation_value' ],
   ),
   v.forward(
-    v.check((input) => !(Boolean(input.string_annotation_value.length) && !input.string_annotation_key.length)),
+    v.check((input) => !(input.string_annotation_value && !input.string_annotation_key)),
     [ 'string_annotation_key' ],
   ),
 );
