315f9f3e0ad258e38449ba56772acaaef1e9fdcc
Bartosz Buczkowski <bartosz.r.buczkowski@gmail.com>
2025-10-09 11:05:45 +0200

    feature/GBBE-251 Replace zod for existing valibot

--- ui/address/addressOwnedFilterSchema.ts
+++ ui/address/addressOwnedFilterSchema.ts
@@ -1,47 +1,5 @@
 import * as v from 'valibot';
 
-// export const annotationSchema = v.pipe(
-//   v.object({
-//     numeric_annotation_key: v.optional(v.string()),
-//     numeric_annotation_value: v.optional(v.string()),
-//     string_annotation_key: v.optional(v.string()),
-//     string_annotation_value: v.optional(v.string()),
-//   }),
-//   v.custom(
-//     (data) => {
-//       if (data.numeric_annotation_value && !data.numeric_annotation_key) {
-//         ctx.addIssue({
-//           path: [ 'numeric_annotation_key' ],
-//           message: 'numeric_annotation_key is required when numeric_annotation_value is filled',
-//         });
-//       }
-
-//       if (data.numeric_annotation_key && !data.numeric_annotation_value) {
-//         ctx.addIssue({
-//           path: [ 'numeric_annotation_value' ],
-//           message: 'numeric_annotation_value is required when numeric_annotation_key is filled',
-//         });
-//       }
-
-//       if (data.string_annotation_value && !data.string_annotation_key) {
-//         ctx.addIssue({
-//           path: [ 'string_annotation_key' ],
-//           message: 'string_annotation_key is required when string_annotation_value is filled',
-//         });
-//       }
-
-//       if (data.string_annotation_key && !data.string_annotation_value) {
-//         ctx.addIssue({
-//           path: [ 'string_annotation_value' ],
-//           message: 'string_annotation_value is required when string_annotation_key is filled',
-//         });
-//       }
-
-//       return true;
-//     },
-//   ),
-// );
-
 export const addressOwnedFilterSchema = v.pipe(
   v.object({
     numeric_annotation_key: v.string(),
