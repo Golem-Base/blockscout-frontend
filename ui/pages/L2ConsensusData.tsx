4dbc1a8114e700a5791512e8b2636ff28e332e63
Bartosz Buczkowski <bartosz.r.buczkowski@gmail.com>
2025-11-03 08:57:39 +0100

    feature/GBBE-161 Add L2 Consensus data page

--- ui/pages/L2ConsensusData.tsx
+++ ui/pages/L2ConsensusData.tsx
@@ -1,9 +1,76 @@
 import { Grid } from '@chakra-ui/react';
+import BigNumber from 'bignumber.js';
 import React from 'react';
 
+import type { ConsensusInfoResponse } from '@golembase/l3-indexer-types';
+
 import useApiQuery from 'lib/api/useApiQuery';
+import dayjs from 'lib/date/dayjs';
+import { currencyUnits } from 'lib/units';
+import { WEI } from 'toolkit/utils/consts';
+import EmptySearchResult from 'ui/shared/EmptySearchResult';
 import PageTitle from 'ui/shared/Page/PageTitle';
 import StatsWidget, { type Props as StatsWidgetProps } from 'ui/shared/stats/StatsWidget';
+
+const keysToCheck: Array<keyof ConsensusInfoResponse> = [
+  'finalized_block_number',
+  'unsafe_block_number',
+  'finalized_block_timestamp',
+  'unsafe_block_timestamp',
+  'rollup_gas_price',
+  'rollup_gas_used',
+  'rollup_transaction_fee',
+];
+
+const getStats = (consensusInfo: ConsensusInfoResponse): Array<StatsWidgetProps> => {
+  return [
+    {
+      icon: 'block_full',
+      label: 'Finalized block number',
+      value: consensusInfo.finalized_block_number,
+    },
+    {
+      icon: 'block_countdown_full',
+      label: 'Finalized block timestamp',
+      value: dayjs.unix(Number(consensusInfo.finalized_block_timestamp)).format('lll'),
+    },
+    {
+      icon: 'block_full',
+      label: 'Unsafe block number',
+      value: consensusInfo.unsafe_block_number,
+    },
+    {
+      icon: 'block_countdown_full',
+      label: 'Unsafe block created at',
+      value: dayjs.unix(Number(consensusInfo.unsafe_block_timestamp)).format('lll'),
+    },
+    {
+      icon: 'block_full',
+      label: 'Safe block number',
+      value: consensusInfo.safe_block_number,
+    },
+    {
+      icon: 'block_countdown_full',
+      label: 'Safe block created at',
+      value: dayjs.unix(Number(consensusInfo.safe_block_timestamp)).format('lll'),
+    },
+    {
+      icon: 'gas',
+      label: 'Rollup gas price',
+      value: `${ BigNumber(consensusInfo.rollup_gas_used).dividedBy(WEI).toFixed() } ${ currencyUnits.ether }`,
+    },
+    {
+      icon: 'gas',
+      label: 'Rollup gas used',
+      value: consensusInfo.rollup_gas_used,
+    },
+    {
+      icon: 'donate',
+      label: 'Rollup transaction fee',
+      value: `${ BigNumber(consensusInfo.rollup_transaction_fee).dividedBy(WEI).toFixed() } ${ currencyUnits.ether }`,
+    },
+  ];
+};
 
 const L2ConsensusData = () => {
   const consensusInfoQuery = useApiQuery('golemBaseIndexer:consensusInfo', {
@@ -26,50 +93,16 @@ const L2ConsensusData = () => {
   const isLoading = Boolean(consensusInfoQuery.isPlaceholderData);
 
   const items: Array<StatsWidgetProps> = React.useMemo(() => {
-    if (!consensusInfo) {
-      return [];
-    }
+    if (!consensusInfo) return [];
 
-    const stats: Array<StatsWidgetProps> = [
-      {
-        icon: 'block_full',
-        label: 'Finalized block number',
-        value: consensusInfo.finalized_block_number,
-      },
-      {
-        icon: 'block_full',
-        label: 'Unsafe block number',
-        value: consensusInfo.unsafe_block_number,
-      },
-      {
-        icon: 'block_countdown_full',
-        label: 'Finalized block timestamp',
-        value: consensusInfo.finalized_block_timestamp,
-      },
-      {
-        icon: 'block_countdown_full',
-        label: 'Unsafe block timestamp',
-        value: consensusInfo.unsafe_block_timestamp,
-      },
-      {
-        icon: 'gas',
-        label: 'Rollup gas price',
-        value: consensusInfo.rollup_gas_price,
-      },
-      {
-        icon: 'gas',
-        label: 'Rollup gas used',
-        value: consensusInfo.rollup_gas_used,
-      },
-      {
-        icon: 'donate',
-        label: 'Rollup transaction fee',
-        value: consensusInfo.rollup_transaction_fee,
-      },
-    ];
+    const stats = getStats(consensusInfo);
 
     return stats.filter(Boolean);
   }, [ consensusInfo ]);
+
+  if (keysToCheck.every(value => consensusInfo?.[value] === '0')) {
+    return <EmptySearchResult heading="Data not found" text="No consensus data available yet"/>;
+  }
 
   return (
     <>
