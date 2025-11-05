b310b23db7d941c6870b0fbe0151ed013a8cecb4
Bartosz Buczkowski <bartosz.r.buczkowski@gmail.com>
2025-11-05 11:36:49 +0100

    feature/GBBE-320 Cleanup dashboard current data statistics

--- ui/home/Stats.tsx
+++ ui/home/Stats.tsx
@@ -143,17 +143,17 @@ const Stats = () => {
         href: { pathname: '/blocks' as const },
         isLoading,
       },
-      (statsData?.average_block_time?.value || apiData?.average_block_time) && {
-        id: 'average_block_time' as const,
-        icon: 'clock-light' as const,
-        label: statsData?.average_block_time?.title || 'Average block time',
-        value: `${
-          statsData?.average_block_time?.value ?
-            Number(statsData.average_block_time.value).toFixed(1) :
-            (apiData!.average_block_time / 1000).toFixed(1)
-        }s`,
-        isLoading,
-      },
+      // (statsData?.average_block_time?.value || apiData?.average_block_time) && {
+      //   id: 'average_block_time' as const,
+      //   icon: 'clock-light' as const,
+      //   label: statsData?.average_block_time?.title || 'Average block time',
+      //   value: `${
+      //     statsData?.average_block_time?.value ?
+      //       Number(statsData.average_block_time.value).toFixed(1) :
+      //       (apiData!.average_block_time / 1000).toFixed(1)
+      //   }s`,
+      //   isLoading,
+      // },
       (statsData?.total_transactions?.value || apiData?.total_transactions) && {
         id: 'total_txs' as const,
         icon: 'transactions_slim' as const,
@@ -216,25 +216,11 @@ const Stats = () => {
         href: { pathname: '/epochs/[number]' as const, query: { number: String(apiData.celo.epoch_number) } },
         isLoading,
       },
-      {
-        id: 'chain_id' as const,
-        icon: 'gear_slim' as const,
-        label: 'Chain ID',
-        value: config.chain.id,
-        isLoading,
-      },
       apiData?.total_gas_used && {
         id: 'total_gas_used' as const,
         icon: 'gas' as const,
         label: 'Total gas used',
         value: `${ BigNumber(apiData.total_gas_used).toFormat() } gas`,
-        isLoading,
-      },
-      apiData?.golembase_storage_limit && {
-        id: 'golembase_storage_limit' as const,
-        icon: 'database' as const,
-        label: 'Storage limit',
-        value: formatDataSize(apiData.golembase_storage_limit),
         isLoading,
       },
       apiData?.golembase_used_slots && {
