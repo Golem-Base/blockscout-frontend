ba2070597787bd9dffa0db18485feeccf4ba9407
Mateusz Juszczyk <matixovi@gmail.com>
2025-11-21 09:53:07 +0100

    Fix mock arkiv

--- playwright/fixtures/mockArkiv.ts
+++ playwright/fixtures/mockArkiv.ts
@@ -2,9 +2,9 @@ import type { TestFixture, Page } from '@playwright/test';
 
 interface MockArkivConfig {
   isConnected?: boolean;
-  createEntitiesResponse?: Array<{ entityKey: string }>;
-  updateEntitiesResponse?: Array<{ entityKey: string }>;
-  queryResponse?: Array<{ key: string; storageValue: Uint8Array }>;
+  createEntityResponse?: Array<{ entityKey: string }>;
+  updateEntityResponse?: Array<{ entityKey: string }>;
+  queryResponse?: Array<{ key: string }>;
 }
 
 export type MockArkivFixture = (config?: MockArkivConfig) => Promise<void>;
@@ -12,11 +12,11 @@ export type MockArkivFixture = (config?: MockArkivConfig) => Promise<void>;
 const fixture: TestFixture<MockArkivFixture, { page: Page }> = async({ page }, use) => {
   await use(async(config = {}) => {
     const defaultEntityKey = '0x0137cd898a9aaa92bbe94999d2a98241f5eabc829d9354160061789963f85995';
-    const golemBaseConfig = {
+    const golemBaseConfig: MockArkivConfig = {
       isConnected: false,
       createEntityResponse: [ { entityKey: defaultEntityKey } ],
       updateEntityResponse: [ { entityKey: defaultEntityKey } ],
-      queryEntitiesResponse: [],
+      queryResponse: [],
       ...config,
     };
 
