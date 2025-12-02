24a7faa99594a1b96a59a138a0ec10dbfd0cf5ef
Bartosz Buczkowski <bartosz.r.buczkowski@gmail.com>
2025-12-02 08:47:43 +0100

    Update snapshots

--- ui/entity/ExtendEntityForm.pw.tsx
+++ ui/entity/ExtendEntityForm.pw.tsx
@@ -8,13 +8,13 @@ import ExtendEntityForm from './ExtendEntityForm';
 test('default view +@dark-mode +@mobile', async({ render, mockArkiv }) => {
   await mockArkiv({ isConnected: true });
 
-  const component = await render(<ExtendEntityForm initialExpiresAtTimestamp={ dayjs().toJSON() }/>);
+  const component = await render(<ExtendEntityForm initialExpiresAtTimestamp={ dayjs('2024-06-15T10:30').toJSON() }/>);
 
   await expect(component).toHaveScreenshot();
 });
 
 test('not connected to Golem Base', async({ render }) => {
-  const component = await render(<ExtendEntityForm initialExpiresAtTimestamp={ dayjs().toJSON() }/>);
+  const component = await render(<ExtendEntityForm initialExpiresAtTimestamp={ dayjs('2024-06-15T10:30').toJSON() }/>);
 
   await expect(component.getByText('Not connected to Golem Base')).toBeVisible();
   await expect(component).toHaveScreenshot();
