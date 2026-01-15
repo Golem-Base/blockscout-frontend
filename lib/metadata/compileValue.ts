6ed7db6755b58fa9a5447fe7759bcd8e6a6c8922
Mateusz Juszczyk <matixovi@gmail.com>
2026-01-15 11:56:51 +0100

    Fix toolkit ts issues

--- lib/metadata/compileValue.ts
+++ lib/metadata/compileValue.ts
@@ -1,6 +1,6 @@
 export default function compileValue(template: string, params: Record<string, string | Array<string> | undefined>) {
   const PLACEHOLDER_REGEX = /%(\w+)%/g;
-  return template.replaceAll(PLACEHOLDER_REGEX, (match, p1) => {
+  return template.replaceAll(PLACEHOLDER_REGEX, (_match, p1) => {
     const value = params[p1];
 
     if (Array.isArray(value)) {
